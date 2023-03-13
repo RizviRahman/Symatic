2abf3de28922f233be19df96641b590db72884ec
Rizvi Rahman Omee <rizvi_rahman_01@yahoo.com>
2023-03-13 23:09:15 +0600

    Project environment setup 1

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
+// const session = require("express-session");
+const path = require("path");
+const dotenv = require("dotenv");
+
 
 const app = express();
 
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/',(req,res)=>{
-    res.send("home");
+    res.render("index");
 });
 
 app.listen(3000, (req, res)=>{
